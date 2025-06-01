28e987b40d04e15bbca1170841d069a6f17f2b7a
Alkotras94 <vagin.ewgen@yandex.ru>
2025-06-01 21:36:26 +0500

    SaveWin

---
+++ Assets/CodeBase/Enemy/AgentMoveToPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace CodeBase.Enemy
+{
+    public class AgentMoveToPlayer : MonoBehaviour
+    {
+        private NavMeshAgent _agent;
+        void Start()	{
+            _agent = GetComponent<NavMeshAgent>();
+            _agent.updateRotation = false;
+            _agent.updateUpAxis = false;
+        }
+
+        private void Update()
+        {
+            PositionFixationZ();
+        }
+
+        private void PositionFixationZ()
+        {
+            Vector3 pos = transform.position;
+            pos.z = 0f; 
+            transform.position = pos;
+        }
+    }
+}
