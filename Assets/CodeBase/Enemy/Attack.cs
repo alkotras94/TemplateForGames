721bfd29f976f13cfc0a125074cabd92be53c743
Alkotras94 <vagin.ewgen@yandex.ru>
2025-06-10 20:47:05 +0500

    Save Win

--- Assets/CodeBase/Enemy/Attack.cs
+++ Assets/CodeBase/Enemy/Attack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Assets.CodeBase.Infrastructure.Factory;
 using Assets.CodeBase.Infrastructure.Services;
 using UnityEngine;
@@ -14,11 +15,18 @@ namespace CodeBase.Enemy
         private Transform _heroTransform;
         private float _attackCooldown;
         private bool _isAttacking;
+        private Collider2D[] _hits = new Collider2D[1];
+        private float Cleavage = 1f;
+        private int _layerMask;
+        private float EfectiveDistance;
+        private bool _attackIsActive = true;
 
         private void Awake()
         {
             _gameFactory = AllServices.Container.Single<IGameFactory>();
             _gameFactory.HeroCreated += OnHeroCreated;
+
+            Cleavage = 1 << LayerMask.NameToLayer("Player");
         }
 
         private void Update()
@@ -32,12 +40,30 @@ namespace CodeBase.Enemy
         private void StartAttack()
         {
             transform.LookAt(_heroTransform); //Может быть ошибка так как у меня 2д игра
-            Animator.PlayIdle(); //Пока что вместо атаки вызываем состояние ожидания
+            Animator.PlayAttack(); //Пока что вместо атаки вызываем состояние ожидания
             
             _isAttacking = true;
         }
 
-        private void OnAttack(){}
+        private void OnAttack()
+        {
+            if (Hit(out Collider2D hit))
+            {
+                PhysicsDebug.DrowDebug(StartPoin(), Cleavage, 2);
+            }    
+        }
+
+        private bool Hit(out Collider2D hit)
+        {
+            Vector2 startPosition = StartPoin();
+            int hitsCount = Physics2D.OverlapCircleNonAlloc(startPosition, Cleavage, _hits, _layerMask);
+            hit = _hits.FirstOrDefault();
+            
+            return hitsCount > 0;
+        }
+
+        private Vector2 StartPoin() => 
+            new Vector2(transform.position.x, transform.position.y) * transform.forward * EfectiveDistance;
 
         private void OnAttackEnded()
         {
@@ -50,8 +76,17 @@ namespace CodeBase.Enemy
                 _attackCooldown -= Time.deltaTime;
         }
         
-        private bool CanAttack() => _isAttacking && CooldownIsUp();
-        private bool CooldownIsUp() => _attackCooldown <= 0f;
-        private void OnHeroCreated() => _heroTransform = _gameFactory.HeroGameObject.transform;
+        private bool CanAttack() => 
+            _attackIsActive && _isAttacking && CooldownIsUp();
+        private bool CooldownIsUp() => 
+            _attackCooldown <= 0f;
+        private void OnHeroCreated() => 
+            _heroTransform = _gameFactory.HeroGameObject.transform;
+
+        public void EnableAttack() => 
+            _attackIsActive = true;
+
+        public void DisableAttack() => 
+            _attackIsActive = false;
     }
 }
---
+++ Assets/CodeBase/Enemy/PhysicsDebug.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+namespace CodeBase.Enemy
+{
+    public static class PhysicsDebug
+    {
+        public static void DrowDebug(Vector2 worldPos, float radius, float second)
+        {
+            Debug.DrawRay(worldPos, radius * Vector2.up,Color.red, second);
+            Debug.DrawRay(worldPos, radius * Vector2.down,Color.red, second);
+            Debug.DrawRay(worldPos, radius * Vector2.left,Color.red, second);
+            Debug.DrawRay(worldPos, radius * Vector2.right,Color.red, second);
+        }
+    }
+}
